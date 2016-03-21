56e715d7aed5d23083b9c7527a1d2dd62a07faf3
Andrii Katsiubka <akatsyubka@gmail.com>
2016-03-21 10:57:00 +0200

    Added missing files

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/Export/WebParts/IWebPartTokenizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Export.WebParts
+{
+    interface IWebPartTokenizer
+    {
+        string Tokenize(string xml, TokenParser parser);
+    }
+}
---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/Export/WebParts/V2/V2WebPartTokenizerManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Export.WebParts.V2
+{
+    class V2WebPartTokenizerManager
+    {
+        public static IWebPartTokenizer GetWebPartTokenizer(string webPartType) {
+            switch (webPartType)
+            {
+                default:
+                    return new V2DefaultWebPartTokenizer();
+                    break;
+            }
+        }
+    }
+}
