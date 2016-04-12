b5d6cd138ae08dffb8e2057aaf296dd38fa0499f
ystetsyuk <y.stetsyuk@abtollc.com>
2016-04-12 12:32:03 +0300

    Added list files provisioning event

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectFiles.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectFiles.cs
@@ -415,7 +415,17 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
             using (var scope = new PnPMonitoredScope(this.Name))
             {
                 var connector = template.Connector;
-                var providers = this.GetUrlProviders(template.Lists);
+
+                List<ListInstance> lists = new List<ListInstance>();
+                foreach (ListInstance list in template.Lists)
+                {
+                    if (creationInfo.ExecutePreProvisionEvent<ListInstance, List>(Handlers.Files, template, list, null))
+                    {
+                        lists.Add(list);
+                    }
+                }
+
+                var providers = this.GetUrlProviders(lists);
                 var files = new OfficeDevPnP.Core.Framework.Provisioning.Model.FileCollection(template);
                 var modelProvider = new FileModelProvider(web, connector);
                 foreach (var provider in providers)
@@ -453,7 +463,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
             return template;
         }
 
-        private System.Collections.Generic.List<IUrlProvider> GetUrlProviders(ListInstanceCollection templateLists)
+        private System.Collections.Generic.List<IUrlProvider> GetUrlProviders(IList<ListInstance> templateLists)
         {
             var result = new List<IUrlProvider>();
             var forms = templateLists.SelectMany(x => x.Forms);
